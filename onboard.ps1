bbe559bad554a98e66a4e94d2362123fd0230984
zukako <shizhong.hangxi@gmail.com>
2024-02-10 11:33:53 +0900

    add error handling

--- onboard.ps1
+++ onboard.ps1
@@ -8,15 +8,17 @@ $cloudbaseAppSpObjId = "22d729db-95de-4fd1-9d8d-dfe8d55e2fec"
 # アタッチしたいRBACロール定義を取得
 $cloudbaseRoleURIs = @("https://cbroledefpublic.blob.core.windows.net/roledefinition/basicRole.json","https://cbroledefpublic.blob.core.windows.net/roledefinition/vmscanRole.json")
 
+# 不要なロールを削除するかどうか
+$needlessRoleDeletionInput = Read-Host "Do you want to delete the needless roles? (yes/no)"
+$needlessRoleDeletionFlag = $needlessRoleDeletionInput -eq "yes"
+
 # アタッチしたいRBACロール定義の名前を取得
 $desiredRoleNames = @()
 foreach($cloudbaseRoleURI in $cloudbaseRoleURIs){
 	Write-Host "Getting Role Definition from $cloudbaseRoleURI"
-	# CloudShell の場合 .Content 不要
+	# ロール定義のJSONを取得
 	$cloudbaseRoleDefinition = $(curl $cloudbaseRoleURI) | ConvertFrom-Json
-	
-	## CloudShell 以外の場合 .Content が必要
-	# $cloudbaseRoleDefinition = $(curl $cloudbaseRoleURI).Content | ConvertFrom-Json 
+
 	$desiredRoleNames += $cloudbaseRoleDefinition.Name
 	$desiredRoleURIs += $cloudbaseRoleURI
 }
@@ -38,57 +40,84 @@ Write-Host "Missing Roles: $missingRoleNames"
 $needlessRoleNames = $attachedRoleNames | Where-Object { $desiredRoleNames -notcontains $_ }
 Write-Host "Needless Roles: $needlessRoleNames"
 
+# 追加で割り当てるべきロールが存在する($missingRoleNamesが$nullではない)場合
 # 割り当てるべきRBACロール定義のURIを改めて取得
-$missingRoleURIs = @()
-$missingRoleDefinitions = @()
-foreach($cloudbaseRoleURI in $cloudbaseRoleURIs){
-	$cloudbaseRoleDefinition = $(curl $cloudbaseRoleURI) | ConvertFrom-Json
-	if ($missingRoleNames.contains($cloudbaseRoleDefinition.Name)){
-		$missingRoleURIs += $cloudbaseRoleURI
-		$missingRoleDefinitions += $cloudbaseRoleDefinition
+if ($missingRoleNames -ne $null)
+{
+	$missingRoleURIs = @()
+	$missingRoleDefinitions = @()
+	foreach($cloudbaseRoleURI in $cloudbaseRoleURIs){
+		$cloudbaseRoleDefinition = $(curl $cloudbaseRoleURI) | ConvertFrom-Json
+		
+		# cloudbaseRoleDefinitionには、既にアタッチされているロールも含まれる
+		# よって$missingRoleNamesに含まれるロールのみを取得する
+		if ($missingRoleNames.contains($cloudbaseRoleDefinition.Name)){
+			$missingRoleURIs += $cloudbaseRoleURI
+			$missingRoleDefinitions += $cloudbaseRoleDefinition
+		}
 	}
-}
-Write-Host "Missing Role URIs: $missingRoleURIs"
-# Write-Host "Missing Role Definitions: $missingRoledDefinitions"
+	Write-Host "Missing Role URIs: $missingRoleURIs"
 
-# assignable scopeのsubscriptionを書き換える処理
-foreach($missingRoleDefinition in $missingRoleDefinitions){
 	# assignable scopeのsubscriptionを書き換える処理
-	$missingRoleDefinition.AssignableScopes = @("/subscriptions/$subscriptionId")
-	# write-host $missingRoleDefinition.AssignableScopes
-	# write-host $missingRoleDefinition | ConvertTo-Json
+	foreach($missingRoleDefinition in $missingRoleDefinitions){
+		# assignable scopeのsubscriptionを書き換える処理
+		$missingRoleDefinition.AssignableScopes = @("/subscriptions/$subscriptionId")
 
-	# ロール名の保持
-	$roleName = $missingRoleDefinition.Name
+		# ロール名の保持
+		$roleName = $missingRoleDefinition.Name
 
-	# jsonファイルの作成
-	$cloudbaseRoleFile = "$($missingRoleDefinition.Name).json"
-	write-host "Creating $cloudbaseRoleFile"
+		# jsonファイルの作成
+		$cloudbaseRoleFile = "$($missingRoleDefinition.Name).json"
+		write-host "Creating $cloudbaseRoleFile"
 
-	# 作成したJSONファイルに書き込み
-	Set-Content $cloudbaseRoleFile $($missingRoleDefinition | ConvertTo-Json)
+		# 作成したJSONファイルに書き込み
+		Set-Content $cloudbaseRoleFile $($missingRoleDefinition | ConvertTo-Json)
 
-	# 作成したJSONファイルからカスタムロール定義を作成
-	# ※既に作成されていた場合はエラーになるので、エラーハンドリングが必要
-	New-AzRoleDefinition -InputFile ./$cloudbaseRoleFile
+		# 作成したJSONファイルからカスタムロール定義を作成
+		# 既に作成されていた場合はエラーになるため、ロールが存在しない場合のみ作成する
+		if(!(Get-AzRoleDefinition -Name $roleName)){
+			New-AzRoleDefinition -InputFile ./$cloudbaseRoleFile
+		}
+		else{
+			Write-Host "Role Definition: $roleName has already existed!"
+		}
+		# カスタムロールの作成に時間がかかるので、作成したロールが使えるようになるまで待つ
+		# カスタムロールがAzure側に反映されるまで時間がかかる場合があるので、ここをループする
+		while(!(Get-AzRoleDefinition -Name $roleName)){
+			Write-Host "Creating Custom Azure RBAC Role: $roleName ..."
+			Start-Sleep -Seconds 3
+		}
+		Write-Host "Custom Azure RBAC Role: $roleName has been created!"
 
-	
-	# カスタムロールの作成に時間がかかるので、作成したロールが使えるようになるまで待つ
-	# カスタムロールがAzure側に反映されるまで時間がかかる場合があるので、ここをループする
-	while(!(Get-AzRoleDefinition -Name $roleName)){
-		Write-Host "Creating Custom Azure RBAC Role: $roleName ..."
-		Start-Sleep -Seconds 3
+		# 作成完了したロールの割り当て
+		New-AzRoleAssignment -ObjectId $cloudbaseAppSpObjId -RoleDefinitionName $roleName -Scope "/subscriptions/$subscriptionId"
+		Write-Host "Custom Azure RBAC Role: $roleName has attached to $cloudbaseAppSpObjId !"
 	}
-	Write-Host "Custom Azure RBAC Role: $roleName has created!"
-
-	# 作成完了したロールの割り当て
-	New-AzRoleAssignment -ObjectId $cloudbaseAppSpObjId -RoleDefinitionName $roleName -Scope "/subscriptions/$subscriptionId"
-	Write-Host "Custom Azure RBAC Role: $roleName has attached to $cloudbaseAppSpObjId !"
+}
+else
+{
+	Write-Host "There is no missing roles to attach."
 }
 
-# 剥奪するべきロールの剥奪
-foreach($needlessRoleName in $needlessRoleNames){
-	Remove-AzRoleAssignment -ObjectId $cloudbaseAppSpObjId -RoleDefinitionName $needlessRoleName
+# 剥奪するべきロールが存在する($needlessRoleNamesが$nullではない)場合
+if ($needlessRoleNames -ne $null)
+{
+	# 剥奪するべきロールの剥奪
+	# 最初にユーザーの入力で剥奪したロールの削除要否を確認する
+	foreach($needlessRoleName in $needlessRoleNames){
+		Remove-AzRoleAssignment -ObjectId $cloudbaseAppSpObjId -RoleDefinitionName $needlessRoleName
+		# 剥奪完了したロールの削除
+		# 削除するロールが他のプリンシパルに割り当てられていた場合は削除できないため、エラーハンドリング(未対応)
+		if($needlessRoleDeletionFlag && (Get-AzRoleDefinition -Name $needlessRoleName).IsCustom){
+			# -Forceを付けないとユーザーへの確認が求められる
+			Remove-AzRoleDefinition -Name $needlessRoleName -Force
+			Write-Host "Custom Azure RBAC Role: $needlessRoleName has been deleted!"
+		}
+	}
+}
+else
+{
+	Write-Host "There is no needless roles to detach."
 }
 
-Write-Host Get-AzRoleAssignment -ObjectId $cloudbaseAppSpObjId
+Get-AzRoleAssignment -ObjectId $cloudbaseAppSpObjId
